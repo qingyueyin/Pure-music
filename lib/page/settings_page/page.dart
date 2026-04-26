bf3d93f39ed5cfeb54f8b1c048c03b42d93ffae9
qingyueyin <1870592250@qq.com>
2026-04-26 23:32:22 +0800

    refactor: 优化设置页面

--- lib/page/settings_page/page.dart
+++ lib/page/settings_page/page.dart
@@ -2,7 +2,6 @@ import 'package:pure_music/page/page_scaffold.dart';
 import 'package:pure_music/page/settings_page/artist_separator_editor.dart';
 import 'package:pure_music/page/settings_page/check_update.dart';
 import 'package:pure_music/page/settings_page/create_issue.dart';
-import 'package:pure_music/page/settings_page/lyric_effect_settings.dart';
 import 'package:pure_music/page/settings_page/other_settings.dart';
 import 'package:pure_music/page/settings_page/theme_settings.dart';
 import 'package:flutter/material.dart';
@@ -45,7 +44,6 @@ class SettingsPage extends StatelessWidget {
           DefaultLyricSourceControl(),
           const SizedBox(height: 24.0),
           const _SettingsSectionHeader("播放"),
-          const EqBypassSwitch(),
           const SizedBox(height: 24.0),
           const _SettingsSectionHeader("外观"),
           DynamicThemeSwitch(),
@@ -55,8 +53,6 @@ class SettingsPage extends StatelessWidget {
           const AppearanceAdvancedSettingsTile(),
           const SizedBox(height: 16.0),
           const NowPlayingBackgroundModeToggle(),
-          const SizedBox(height: 16.0),
-          const LyricEffectsSettingsTile(),
           const SizedBox(height: 24.0),
           const _SettingsSectionHeader("高级与关于"),
           ArtistSeparatorEditor(),
