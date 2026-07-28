cc0c1ca04ca6c17dfee6de405e5460e604179a8e
qingyueyin <1870592250@qq.com>
2026-07-28 15:31:56 +0800

    feat(nav): 统计页入口

--- lib/component/side_nav.dart
+++ lib/component/side_nav.dart
@@ -25,6 +25,7 @@ final destinations = <DestinationDesc>[
   DestinationDesc(Symbols.album, '专辑', app_paths.ALBUMS_PAGE),
   DestinationDesc(Symbols.folder, '文件夹', app_paths.FOLDERS_PAGE),
   DestinationDesc(Symbols.list, '歌单', app_paths.PLAYLISTS_PAGE),
+  DestinationDesc(Symbols.bar_chart, '统计', app_paths.STATS_PAGE),
   DestinationDesc(Symbols.settings, '设置', app_paths.SETTINGS_PAGE),
 ];
 
